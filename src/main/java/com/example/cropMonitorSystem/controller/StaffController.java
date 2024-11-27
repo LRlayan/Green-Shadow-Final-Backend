66eae703e95e9f28e3dfca10f55aa974df81e86a
LRLayan <rameshlayan4@gmail.com>
2024-11-28 01:09:25 +0530

    StaffController

---
+++ src/main/java/com/example/cropMonitorSystem/controller/StaffController.java
@@ -0,0 +1,83 @@
+package com.example.cropMonitorSystem.controller;
+
+import com.example.cropMonitorSystem.customStatusCode.SelectedErrorStatus;
+import com.example.cropMonitorSystem.dto.StaffStatus;
+import com.example.cropMonitorSystem.dto.impl.StaffDTO;
+import com.example.cropMonitorSystem.exception.DataPersistException;
+import com.example.cropMonitorSystem.exception.StaffNotFoundException;
+import com.example.cropMonitorSystem.service.StaffService;
+import com.example.cropMonitorSystem.util.Regex;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/staff")
+@CrossOrigin
+public class StaffController {
+    @Autowired
+    private StaffService staffService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveStaff(@RequestBody StaffDTO staffDTO){
+        try{
+            if (!Regex.emailValidator(staffDTO.getEmail()).matches()){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            staffService.saveStaffMember(staffDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/{staffId}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public StaffStatus getSelectedStaff(@PathVariable("staffId") String staffId){
+        if (!Regex.idValidator(staffId).matches()){
+            return new SelectedErrorStatus(1,"Staff Code Not Valid");
+        }
+        return staffService.getSelectedStaffMember(staffId);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<StaffDTO> getAllStaff(){
+        return staffService.getAllStaffMember();
+    }
+
+    @DeleteMapping(value = "/{staffId}")
+    public ResponseEntity<Void> deleteStaff(@PathVariable ("staffId") String staffId){
+        try{
+            if (!Regex.idValidator(staffId).matches()){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            staffService.deleteStaffMember(staffId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (StaffNotFoundException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping(value = "/{staffId}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateStaff(@PathVariable ("staffId") String staffId ,@RequestBody StaffDTO staffDTO){
+        try{
+            staffService.updateStaffMember(staffId,staffDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
