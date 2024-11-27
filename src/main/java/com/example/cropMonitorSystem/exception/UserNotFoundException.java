65fc1a5bce37620895ca7194e9ad3edbfbdcf549
LRLayan <rameshlayan4@gmail.com>
2024-11-28 01:11:53 +0530

    UserNotFoundException

---
+++ src/main/java/com/example/cropMonitorSystem/exception/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.cropMonitorSystem.exception;
+
+public class UserNotFoundException {
+    public UserNotFoundException() {
+
+    }
+
+    public UserNotFoundException(String message) {
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+    }
+}
