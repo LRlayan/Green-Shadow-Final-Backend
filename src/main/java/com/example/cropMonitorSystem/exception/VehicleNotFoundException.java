f32a1952f1d54b3fd269c11200df097a2fe1eb0c
LRLayan <rameshlayan4@gmail.com>
2024-11-28 01:13:16 +0530

    VehicleNotFoundException

---
+++ src/main/java/com/example/cropMonitorSystem/exception/VehicleNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.cropMonitorSystem.exception;
+
+public class VehicleNotFoundException extends Throwable {
+    public VehicleNotFoundException() {
+
+    }
+
+    public VehicleNotFoundException(String message) {
+    }
+
+    public VehicleNotFoundException(String message, Throwable cause) {
+    }
+}
