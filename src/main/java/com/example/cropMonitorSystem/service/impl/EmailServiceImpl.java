7ec48415480f10dfcec81cfd0d0770193735a44a
LRLayan <rameshlayan4@gmail.com>
2024-11-28 01:15:18 +0530

    EmailServiceImpl

---
+++ src/main/java/com/example/cropMonitorSystem/service/impl/EmailServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.cropMonitorSystem.service.impl;
+
+import com.example.cropMonitorSystem.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    @Override
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        message.setFrom("rameshlayan4@gmail.com"); // Optional: explicitly set "from" email address
+        javaMailSender.send(message);
+    }
+}
