33d124d1a90e11d1482f4a020ff144c810b7ad6f
LRLayan <rameshlayan4@gmail.com>
2024-11-28 01:08:18 +0530

    LogNotFoundException

---
+++ src/main/java/com/example/cropMonitorSystem/exception/LogNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.cropMonitorSystem.exception;
+
+public class LogNotFoundException {
+    public LogNotFoundException() {
+
+    }
+
+    public LogNotFoundException(String message) {
+    }
+
+    public LogNotFoundException(String message, Throwable cause) {
+    }
+}
