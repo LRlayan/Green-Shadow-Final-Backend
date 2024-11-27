866d60bc841c33d1979e0733306c544052114be7
LRLayan <rameshlayan4@gmail.com>
2024-11-28 01:10:01 +0530

    StaffNotFoundException

---
+++ src/main/java/com/example/cropMonitorSystem/exception/StaffNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.cropMonitorSystem.exception;
+
+public class StaffNotFoundException extends Throwable {
+    public StaffNotFoundException() {
+
+    }
+
+    public StaffNotFoundException(String message) {
+    }
+
+    public StaffNotFoundException(String message, Throwable cause) {
+    }
+}
