9a2a26a5e9c48e7262b567c742779e17a982f689
LRLayan <rameshlayan4@gmail.com>
2024-11-28 01:16:42 +0530

    JwtService

---
+++ src/main/java/com/example/cropMonitorSystem/service/JwtService.java
@@ -0,0 +1,12 @@
+package com.example.cropMonitorSystem.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface JwtService {
+
+    String extractUserName(String token);
+    boolean isTokenValid(String token, UserDetails userDetails);
+
+    String generateToken(UserDetails userDetails);
+    String refreshToken(UserDetails userDetails);
+}
