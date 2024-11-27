2c57464df3024fd1584299b6ea1338b1e1c4ada9
LRLayan <rameshlayan4@gmail.com>
2024-11-28 01:20:55 +0530

    UserWithKey

---
+++ src/main/java/com/example/cropMonitorSystem/dto/impl/UserWithKey.java
@@ -0,0 +1,18 @@
+package com.example.cropMonitorSystem.dto.impl;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserWithKey {
+    @NotNull
+    @Email
+    private String email;
+    @NotNull
+    private String code;
+}
