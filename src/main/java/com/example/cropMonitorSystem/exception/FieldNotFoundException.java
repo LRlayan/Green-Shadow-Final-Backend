5556238167360ffa1dc2801aff4b62b099d7f648
LRLayan <rameshlayan4@gmail.com>
2024-11-28 01:03:19 +0530

    Fie

---
+++ src/main/java/com/example/cropMonitorSystem/exception/FieldNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.cropMonitorSystem.exception;
+
+public class FieldNotFoundException extends Throwable {
+    public FieldNotFoundException() {
+
+    }
+
+    public FieldNotFoundException(String message) {
+    }
+
+    public FieldNotFoundException(String message, Throwable cause) {
+    }
+}
