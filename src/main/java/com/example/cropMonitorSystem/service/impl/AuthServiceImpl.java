f1fb0777e43961bfd4f6f2d198169c66cf387bd8
LRLayan <rameshlayan4@gmail.com>
2024-12-02 03:37:36 +0530

    refactor class name

---
+++ src/main/java/com/example/cropMonitorSystem/service/impl/AuthServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.cropMonitorSystem.service.impl;
+
+import com.example.cropMonitorSystem.dao.UserDAO;
+import com.example.cropMonitorSystem.dto.impl.UserWithKey;
+import com.example.cropMonitorSystem.entity.impl.UserEntity;
+import com.example.cropMonitorSystem.service.EmailService;
+import com.example.cropMonitorSystem.service.AuthService;
+import com.example.cropMonitorSystem.util.AppUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+    private final UserDAO userDAO;
+    private final EmailService emailService;
+
+    @Override
+    public UserDetailsService userDetailsService() {
+        return username ->
+                userDAO.findByEmail(username).
+                        orElseThrow(()->new UsernameNotFoundException("User Not Found"));
+
+    }
+
+    @Override
+    public boolean sendCodeToChangePassword(UserWithKey userWithKey) {
+        Optional<UserEntity> byEmail = userDAO.findByEmail(userWithKey.getEmail());
+        if (byEmail.isPresent()){
+            emailService.sendEmail(userWithKey.getEmail(),"Your password change Code From Green Shadow(PVT) Ltd.","Dont share with anyone:  "+userWithKey.getCode());
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getOTP() {
+        return AppUtil.getOTP();
+    }
+}
