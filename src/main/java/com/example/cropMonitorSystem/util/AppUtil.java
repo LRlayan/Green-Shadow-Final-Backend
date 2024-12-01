c836456eec38e34602c4404a9e4d22d18832a339
LRLayan <rameshlayan4@gmail.com>
2024-12-01 12:54:17 +0530

    getOTP()

--- src/main/java/com/example/cropMonitorSystem/util/AppUtil.java
+++ src/main/java/com/example/cropMonitorSystem/util/AppUtil.java
@@ -1,9 +1,16 @@
 package com.example.cropMonitorSystem.util;
 
 import java.util.Base64;
+import java.util.Random;
 
 public class AppUtil {
     public static String imageBase64(byte[] image){
         return Base64.getEncoder().encodeToString(image);
+    }
+    public static String getOTP(){
+        Random random = new Random();
+        // Generate a random 4-digit number
+        int otp = 1000 + random.nextInt(9000);
+        return String.valueOf(otp);
     }
 }
