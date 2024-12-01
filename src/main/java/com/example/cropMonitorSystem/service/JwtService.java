a42ab3ba53155d200f7163e148fe7a140f0108e7
LRLayan <rameshlayan4@gmail.com>
2024-12-02 03:56:27 +0530

    refactor code format

--- src/main/java/com/example/cropMonitorSystem/service/JwtService.java
+++ src/main/java/com/example/cropMonitorSystem/service/JwtService.java
@@ -3,10 +3,8 @@ package com.example.cropMonitorSystem.service;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface JwtService {
-
     String extractUserName(String token);
     boolean isTokenValid(String token, UserDetails userDetails);
-
     String generateToken(UserDetails userDetails);
     String refreshToken(UserDetails userDetails);
 }
