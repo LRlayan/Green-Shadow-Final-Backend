ac75ebcef4917bfd5ab073e1c3d4d1d14d728938
LRLayan <rameshlayan4@gmail.com>
2024-12-02 03:47:43 +0530

    refactor code format

--- src/main/java/com/example/cropMonitorSystem/service/AuthenticationService.java
+++ src/main/java/com/example/cropMonitorSystem/service/AuthenticationService.java
@@ -6,10 +6,8 @@ import com.example.cropMonitorSystem.secureAndResponse.secure.SignIn;
 import com.example.cropMonitorSystem.secureAndResponse.secure.SignUp;
 
 public interface AuthenticationService {
-
     JwtAuthResponse signUp(SignUp signUp);
     JwtAuthResponse signIn(SignIn signIn);
-
     JwtAuthResponse refreshToken(String refreshToken);
     void changePassword(ChangePasswordDTO changePasswordDTO);
 }
