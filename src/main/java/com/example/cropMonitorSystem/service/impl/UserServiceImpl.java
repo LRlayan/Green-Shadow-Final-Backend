85ba95e130d02b35972d32f62675294c9701bd2d
LRLayan <rameshlayan4@gmail.com>
2024-12-02 03:54:25 +0530

    getAllUser();

--- src/main/java/com/example/cropMonitorSystem/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/cropMonitorSystem/service/impl/UserServiceImpl.java
@@ -1,46 +1,24 @@
 package com.example.cropMonitorSystem.service.impl;
 
 import com.example.cropMonitorSystem.dao.UserDAO;
-import com.example.cropMonitorSystem.dto.impl.UserWithKey;
-import com.example.cropMonitorSystem.entity.impl.UserEntity;
-import com.example.cropMonitorSystem.service.EmailService;
+import com.example.cropMonitorSystem.dto.impl.UserDTO;
 import com.example.cropMonitorSystem.service.UserService;
-import com.example.cropMonitorSystem.util.AppUtil;
+import com.example.cropMonitorSystem.util.Mapping;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserDAO userDAO;
-    private final EmailService emailService;
+    private final Mapping mapping;
 
     @Override
-    public UserDetailsService userDetailsService() {
-        return username ->
-                userDAO.findByEmail(username).
-                        orElseThrow(()->new UsernameNotFoundException("User Not Found"));
-
-    }
-
-    @Override
-    public boolean sendCodeToChangePassword(UserWithKey userWithKey) {
-        Optional<UserEntity> byEmail = userDAO.findByEmail(userWithKey.getEmail());
-        if (byEmail.isPresent()){
-            emailService.sendEmail(userWithKey.getEmail(),"Your password change Code From Green Shadow(PVT) Ltd.","Dont share with anyone:  "+userWithKey.getCode());
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public String getOTP() {
-        return AppUtil.getOTP();
+    public List<UserDTO> getALlUsers() {
+        return mapping.userList(userDAO.findAll());
     }
 }
