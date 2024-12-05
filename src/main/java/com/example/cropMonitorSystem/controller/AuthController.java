f79dacb6ef44482078b5d2442188679d86216ee8
LRLayan <rameshlayan4@gmail.com>
2024-12-05 12:05:41 +0530

    add logs

--- src/main/java/com/example/cropMonitorSystem/controller/AuthController.java
+++ src/main/java/com/example/cropMonitorSystem/controller/AuthController.java
@@ -10,6 +10,8 @@ import com.example.cropMonitorSystem.service.AuthenticationService;
 import com.example.cropMonitorSystem.service.AuthService;
 import com.example.cropMonitorSystem.util.AppUtil;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthenticationService authenticationService;
     private final AuthService authService;
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     @PostMapping("/signUp")
     public ResponseEntity<JwtAuthResponse> signup(@RequestBody SignUp signup){
@@ -30,6 +33,7 @@ public class AuthController {
         signUp.setEmail(signUp.getEmail());
         signUp.setRole(signUp.getRole());
         signUp.setPassword(AppUtil.temporaryUserPasswordGenerator());
+        logger.info("SignUp saved successfully with email: {}", signUp.getEmail());
         return ResponseEntity.ok(authenticationService.signUp(signup));
     }
 
@@ -46,8 +50,10 @@ public class AuthController {
     @PostMapping(value = "/sendCode")
     public ResponseEntity<Void> sendCode(@RequestBody() UserWithKey userWithKey){
         if (authService.sendCodeToChangePassword(userWithKey)) {
+            logger.info("send code successfully: {}", userWithKey);
             return new ResponseEntity<>(HttpStatus.CREATED);
         }else {
+            logger.error("Error persisting code for forgetPassword: {}" , userWithKey);
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
@@ -62,8 +68,10 @@ public class AuthController {
     public ResponseEntity<Void> changePassword(@RequestBody ChangePasswordDTO changePasswordDTO){
         try {
             authenticationService.changePassword(changePasswordDTO);
+            logger.info("Change password successfully: {}", changePasswordDTO.getEmail());
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (Exception e){
+            logger.error("Unexpected error occurred while saving password {}", e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
